2dc60d0dea7f6a35d3a1b4a63d75a61150b77f21
Manuel S. Lemos <m.lemos@geekshubs.com>
2021-01-12 10:17:34 +0100

    Create Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './style.css';
+
+class Header extends Component {
+
+    render(){
+        return (
+           <header className="component-header">
+               😸 Emoji Searach 😸
+           </header>
+        )
+    }
+}
+
+export default Header;
