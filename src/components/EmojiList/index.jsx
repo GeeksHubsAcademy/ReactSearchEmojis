028c1bf0d42115c3a2c5b67d3a1f900d4ec492c2
Manuel S. Lemos <m.lemos@geekshubs.com>
2021-01-12 12:15:52 +0100

    Clipboard

--- src/components/EmojiList/index.jsx
+++ src/components/EmojiList/index.jsx
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
-
+import Clipboard from "clipboard";
 import EmojiListRow from '../EmojiListRow';
 
 import './style.css';
 
 class EmojiList extends Component {
 
+    componentDidMount() {
+        this.clipboard = new Clipboard(".copy-to-clipboard");
+    }
+
+    componentWillUnmount() {
+        this.clipboard.destroy();
+    }
+
     render() {
         return (
             <div className="component-emoji-list">
                 { this.props.data.map( emoji => (
-                    <EmojiListRow emoji={emoji} />
+                    <EmojiListRow emoji={emoji} key={emoji.id} />
                 )) }
             </div>
         )
--- src/components/EmojiListRow/index.jsx
+++ src/components/EmojiListRow/index.jsx
@@ -3,15 +3,15 @@ import './style.css';
 
 class EmojiListRow extends Component {
 
-    constructor(props){
-        super(props);
-    }
-
     render() {
         return (
-            <div className="component-emoji-row">
+            <div 
+                className="component-emoji-row copy-to-clipboard"
+                data-clipboard-text={this.props.emoji.symbol}
+            >
                 <span className="symbol"> { this.props.emoji.symbol } </span>
                 <span className="title"> { this.props.emoji.title } </span>
+                <span className="info"> Click to copy emoji </span>
             </div>
         )
     }
