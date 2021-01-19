ffb1dd07d52ef5ced1bbe0c45a1721833a633318
Manuel S. Lemos <m.lemos@geekshubs.com>
2021-01-19 10:48:01 +0100

    Implement Redux Thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
 import { rootReducers } from './reducers';
 
-export const store = createStore(rootReducers);
+export const store = createStore(rootReducers, applyMiddleware(thunk));
 
  
